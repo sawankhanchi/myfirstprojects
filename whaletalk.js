7082f7b8ebbc36ba56a5b47afbe3870ec8d1d0a6
Sushil Khanchi <sushilkhanchi97@gmail.com>
2020-04-03 23:08:37 +0530

    it shows vowels

---
+++ whaletalk.js
@@ -0,0 +1,21 @@
+let input = 'turpentine and turtles';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+var resultArray =[];
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+for (let vowel = 0; vowel < vowels.length; vowel++) {
+  if (input[inputIndex] === 
+      vowels[vowel]) {
+    if (input[inputIndex] === 'e') {
+      resultArray.push('ee');
+      
+    }
+    else if (input[inputIndex] === 'u') {
+      resultArray.push('uu');
+    }
+    else {
+      resultArray.push(input[inputIndex]);
+    }
+  }
+}
+}
+console.log(resultArray.join('').toUpperCase());
